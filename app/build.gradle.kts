729c3806dbf6905ae1470baea7ea0fa92165e6c0
Iamzaryab <zaryabdev@gmail.com>
2024-11-19 19:51:31 +0400

    Gradle Build Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
 }
-
 android {
     namespace = "com.zaryabshakir.mediagallery"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.zaryabshakir.mediagallery"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -36,12 +38,21 @@ android {
 }
 
 dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt)
+
+    implementation(libs.glide)
+
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.android.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
 }
